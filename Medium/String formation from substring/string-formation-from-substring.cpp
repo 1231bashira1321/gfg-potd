8558d994ce7df61533b89dcac33ef9df92b887b7
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-08-23 21:51:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/String formation from substring/string-formation-from-substring.cpp
+++ Medium/String formation from substring/string-formation-from-substring.cpp
@@ -11,7 +11,7 @@ public:
 	int isRepeat(string s)
 	{
 	    int n=s.length();
-	    for(int i=n/2 ; i>=1 ; i--){
+	    for(int i=1 ; i<=n/2 ; i++){
 	        
 	        if(n%i==0){ //substr lenght (i) should be multiple of s length
 	             string pattern=s.substr(0,i);
