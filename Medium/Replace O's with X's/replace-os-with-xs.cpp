cdb324487ef8c3c6648a957948cb704eba54568f
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-09-05 06:08:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/Replace O's with X's/replace-os-with-xs.cpp
+++ Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -8,47 +8,43 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
+    void dfs(vector<vector<char>> &mat,int r,int c,vector<vector<int>>&vis){
+     vis[r][c]=1;
+     int dx[]={-1,0,1,0};
+     int dy[]={0,1,0,-1};
+     int n=mat.size();
+     int m=mat[0].size();
+     for(int i=0;i<4;i++){
+         int newr=r+dx[i];
+         int newc=c+dy[i];
+         if(newr>=0 && newr<n && newc>=0 && newc<m && vis[newr][newc]==0 && mat[newr][newc]=='O')
+          {dfs(mat,newr,newc,vis);}
+     }
+    }
 public:
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
-
-    bool isValid(int ii, int jj, int& n, int& m) {
-        return (ii >= 0 && jj >= 0 && ii < n && jj < m); 
-    }
-
-    bool isBoundary(int i, int j, int &n, int &m) {
-        return (i == 0 || j == 0 || i == n - 1 || j == m - 1);
-    }
-
-    void setNotClosed(int i, int j, int& n, int& m, vector<vector<char>>& mat) {
-        mat[i][j] = 'N';
-
-        for (int d = 0; d < 4; ++d) {
-            int ii = dx[d] + i;
-            int jj = dy[d] + j;
-            if (isValid(ii, jj, n, m) && mat[ii][jj] == 'O')
-                setNotClosed(ii, jj, n, m, mat);
-        }
-    }
     vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
-           for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (isBoundary(i, j, n, m) && mat[i][j] == 'O')
-                    setNotClosed(i, j, n, m, mat);
-            }
-        }
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (mat[i][j] == 'O')
-                    mat[i][j] = 'X';
-                else if (mat[i][j] == 'N')
-                    mat[i][j] = 'O';
-            }
-        }
-
-        return mat;
+      //O (or set of O) connected to boundary will not be converted to X
+      vector<vector<int>> vis(n,vector<int>(m,0));
+      for(int i=0;i<m;i++){
+          //first row
+          if(mat[0][i]=='O' && vis[0][i]==0){dfs(mat,0,i,vis);}
+          //last row
+          if(mat[n-1][i]=='O' && vis[n-1][i]==0){dfs(mat,n-1,i,vis);}
+      }
+      for(int i=0;i<n;i++){
+          //first col
+          if(mat[i][0]=='O' && vis[i][0]==0){dfs(mat,i,0,vis);}
+          //last col
+          if(mat[i][m-1]=='O' && vis[i][m-1]==0){dfs(mat,i,m-1,vis);}
+      }
+      //convert
+      for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+              if(vis[i][j]==0 && mat[i][j]=='O'){mat[i][j]='X';}
+          }
+      }
+     return mat; 
     }
 };
 
