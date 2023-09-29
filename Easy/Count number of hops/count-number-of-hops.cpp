4e1c20848434fecb177606a471103b2c439d39d0
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-09-29 19:17:16 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count number of hops/count-number-of-hops.cpp
+++ Easy/Count number of hops/count-number-of-hops.cpp
@@ -5,31 +5,18 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution
-{
+{   long long solve(int n,vector<long long>&dp){
+      if(n<0){return 0;}
+      if(n==0){return 1;}
+      if(dp[n] !=-1){return dp[n];}
+      dp[n]=(solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp))%(1000000007);
+      return dp[n];
+}
     public:
     //Function to count the number of ways in which frog can reach the top.
-    long long countWays(int N)
-    {
-        
-        // your code here
-         vector<long long> dp(4, 0);
-        
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=4;
-        
-        if(N<=3){
-            return dp[N];
-        }
-        
-        for(int i=4; i<=N; ++i){
-            long long ways=(dp[1]+dp[2]+dp[3])%1000000007;
-            dp[1]=dp[2];
-            dp[2]=dp[3];
-            dp[3]=ways;
-        }
-        
-        return dp[3];
+    long long countWays(int n)
+    { vector<long long >dp(n+1,-1);
+      return solve(n,dp);
     }
 };
 
