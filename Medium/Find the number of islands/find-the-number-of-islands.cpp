2ffba9c3862dd4bdca1cde5a0db64370314ae490
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-10-08 12:59:51 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -4,16 +4,27 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    void dfs(int r,int c,vector<vector<int>> &visited,vector<vector<char>>& grid){
+    //isme bhi 1 k sare 1 wale neighbours ko visited mark kr rhe h
+    void bfs(int R,int C,vector<vector<int>> &visited,vector<vector<char>>& grid){
      int n=grid.size();
      int m=grid[0].size();
-     if(r<0 || r==n || c<0 || c==m || visited[r][c]==1 || grid[r][c]=='0'){return ;}
-     int dx[]={-1,-1,0,1,1,1,0,-1};
-     int dy[]={0,1,1,1,0,-1,-1,-1};
-     visited[r][c]=1;
-     for(int i=0;i<8;i++){
-         dfs(r+dx[i],c+dy[i],visited,grid);
-     }
+     queue<pair<int,int>> q;
+     q.push({R,C});
+     while(!q.empty()){
+      int r=q.front().first;
+      int c=q.front().second;
+      q.pop();
+      int dx[]={-1,-1,0,1,1,1,0,-1};
+      int dy[]={0,1,1,1,0,-1,-1,-1};
+      for(int i=0;i<8;i++){
+          int new_row=r+dx[i];
+          int new_col=c+dy[i];
+       if(new_row>=0 && new_row<n && new_col>=0 && new_col<m && !visited[new_row][new_col] && grid[new_row][new_col]=='1'){
+         q.push({new_row,new_col});
+         visited[new_row][new_col]=1;
+       }
+      }
+    }
     }
   public:
     int numIslands(vector<vector<char>>& grid) {
@@ -25,9 +36,9 @@ class Solution {
      vector<vector<int>> visited(n,vector<int>(m,0));
      for(int i=0;i<n;i++){
          for(int j=0;j<m;j++){
-             if(!visited[i][j] &&  grid[i][j]=='1'){
+             if(!visited[i][j] && grid[i][j]=='1'){
                  ans++;
-                 dfs(i,j,visited,grid);
+                 bfs(i,j,visited,grid);
              }
          }
      }
