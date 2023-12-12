2c3dac681cb50b2d17a3af7881bd8fb3e6195ab3
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-12 23:13:53 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find position of set bit/find-position-of-set-bit.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findPosition(int N) {
+     int n=N & (N-1);
+     if(N==0 || n !=0){return -1;}//number is not a power of 2
+     return log2(N)+1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findPosition(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
