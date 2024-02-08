fe00f0c09e3790f0b2a69f4ab5a7ca71c73a5922
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-02-08 23:58:43 +0530

    Updated Solution - GfG to GitHub

--- Basic/Power of 2/power-of-2.cpp
+++ Basic/Power of 2/power-of-2.cpp
@@ -12,11 +12,7 @@ class Solution{
     public:
     // Function to check if given number n is a power of two.
     bool isPowerofTwo(long long n){
-     if(n==0){return false;}
-     n=n & (n-1);
-     if(n==0){return true;}
-     return false;
-        
+      return (n !=0 && (n & n-1)==0)? true : false;
     }
 };
 
